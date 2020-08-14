f71870404f4466dfd2d25c9137e008bf5e3056fb
Jonatan Hernandez <36695143+JonatanHN@users.noreply.github.com>
2020-08-14 14:25:48 -0500

    Add files via upload

---
+++ ugar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+###############################################################################
+#Script Name    : ugar.sh                       
+#Description    : execute multiple commands on multiple servers                                                                     
+#Author         : JonatanHN       
+#Email          : [email protected] 
+################################################################################
+echo 'Ejecuta este archivo con sudo';
+
+apt update; apt upgrade; apt autoremove;
