04bfa03889459442370aebe8aca2b70993bf33d1
JonatanHN <jonapato00@gmail.com>
2023-05-12 19:16:27 -0600

    + code

--- ugar.sh
+++ ugar.sh
@@ -5,37 +5,36 @@
 #Author         : JonatanHN       
 #Email          : [email protected] 
 ################################################################################
-echo '                                                            ' 
+echo '                                                            '
 echo '                                                            '
 echo '------------------------------------------------------------'
-echo '   	$$\   $$\  $$$$$$\   $$$$$$\  $$$$$$$\'  
-echo '   	$$ |  $$ |$$  __$$\ $$  __$$\ $$  __$$\' 
-echo '   	$$ |  $$ |$$ /  \__|$$ /  $$ |$$ |  $$ |'
-echo '   	$$ |  $$ |$$ |$$$$\ $$$$$$$$ |$$$$$$$  |'
-echo '   	$$ |  $$ |$$ |\_$$ |$$  __$$ |$$  __$$< '
-echo '   	$$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |'
-echo '   	\$$$$$$  |\$$$$$$  |$$ |  $$ |$$ |  $$ |'
-echo '   	 \______/  \______/ \__|  \__|\__|  \__|'
+echo '    $$\   $$\  $$$$$$\   $$$$$$\  $$$$$$$\'  
+echo '    $$ |  $$ |$$  __$$\ $$  __$$\ $$  __$$\' 
+echo '    $$ |  $$ |$$ /  \__|$$ /  $$ |$$ |  $$ |'
+echo '    $$ |  $$ |$$ |$$$$\ $$$$$$$$ |$$$$$$$  |'
+echo '    $$ |  $$ |$$ |\_$$ |$$  __$$ |$$  __$$< '
+echo '    $$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |'
+echo '    \$$$$$$  |\$$$$$$  |$$ |  $$ |$$ |  $$ |'
+echo '     \______/  \______/ \__|  \__|\__|  \__|'
 echo '---------------------pwd by JonatanHN-----------------------'
-echo '                                                            ' 
 echo '                                                            '
-
-echo 'EJECUTANDO VERIFICACION DE ACTUALIZACIONES DE DEPENDENCIAS';
-echo '                                                            ' 
 echo '                                                            '
-apt update;
-echo '                                                            ' 
-echo '                                                            ' 
-echo 'EJECUTANDO ACTUALIZACIONES DE DEPENDENCIAS';
-echo '                                                            ' 
+echo 'EJECUTANDO VERIFICACION DE ACTUALIZACIONES DE DEPENDENCIAS'
 echo '                                                            '
-apt upgrade; 
-echo '                                                            ' 
 echo '                                                            '
-echo 'EJECUTANTO AUTOREMOVE PARAR ELIMINAR DEPENDENCIAS QUE ESTEN DUPLICADAS';
-echo '                                                            ' 
+apt update >/dev/null 2>&1
 echo '                                                            '
-apt autoremove;
-echo '                                                            ' 
-echo '                  FINALIZACION DEL SCRIPT                              ';
-echo '                                                            ' 
+echo '                                                            '
+echo 'EJECUTANDO ACTUALIZACIONES DE DEPENDENCIAS'
+echo '                                                            '
+echo '                                                            '
+apt upgrade;
+echo '                                                            '
+echo '                                                            '
+echo 'EJECUTANTO AUTOREMOVE PARA ELIMINAR DEPENDENCIAS DUPLICADAS'
+echo '                                                            '
+echo '                                                            '
+apt autoremove >/dev/null 2>&1
+echo '                                                            '
+echo '                  FINALIZACION DEL SCRIPT                              '
+echo '                                                            '
