946cae8c33b5de91530e60c8de832ca15fbc2c03
Jonatan Hernández <36695143+JonatanHN@users.noreply.github.com>
2023-09-30 00:24:46 -0600

    Add files via upload

--- mcli.sh
+++ mcli.sh
@@ -1,18 +1,40 @@
-echo "/////////////////////////////////////////";
-echo "//       Manager CLI DU & DF          //";
-echo "///////////////////////////////////////";
-echo "\n";
-echo "//          Recursos           //";
-df -h ./;
-echo "\n";
-echo "//      Archivos del sistema       //";
-ls -lha / ;
-echo "\n";
-echo "//      Archivos de carpeta       //";
-ls -lha ./ ;
-echo "\n";
-echo "// Uso de espacio folder actual  //";
-du -sch ./;
-echo "//  Script Desarrollado por JonatanHN  //";
-echo "// ////////////////////////////////// //";
+#!/bin/bash
+###############################################################################
+#Script Name    : ugar.sh                       
+#Description    : execute multiple commands on multiple servers                                                                     
+#Author         : JonatanHN       
+#Email          : [email protected] 
+################################################################################
 
+# Función para mostrar encabezado
+show_header() {
+    echo -e "\n/////////////////////////////////////////"
+    echo -e "//       Manager CLI DU & DF          //"
+    echo -e "///////////////////////////////////////\n"
+}
+
+# Función para mostrar secciones
+show_section() {
+    echo -e "\n[$1]\n"
+}
+
+# Mostrar encabezado
+show_header
+
+# Mostrar recursos
+show_section "Recursos"
+df -h ./
+du -sch ./
+
+# Mostrar archivos del sistema
+show_section "Archivos del sistema"
+ls -lha /
+
+# Mostrar archivos de la carpeta actual
+show_section "Archivos de carpeta"
+ls -lha ./
+
+# Mostrar pie de página
+echo -e "\n/////////////////////////////////////////"
+echo -e "//  Script Desarrollado por JonatanHN  //"
+echo -e "/////////////////////////////////////////\n"
