b540d141f2aa060180f7987314934538c2f46418
Jonatan Hernández <36695143+JonatanHN@users.noreply.github.com>
2023-10-01 00:42:20 -0600

    Add files via upload

--- mcli.sh
+++ mcli.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ###############################################################################
-#Script Name    : ugar.sh                       
+#Script Name    : mcli.sh                       
 #Description    : execute multiple commands on multiple servers                                                                     
 #Author         : JonatanHN       
 #Email          : [email protected] 
@@ -23,18 +23,25 @@ show_header
 
 # Mostrar recursos
 show_section "Recursos"
+
 df -h ./
-du -sch ./
+
+# Mostrar recursos
+show_section "Archivos pesados en /"
+
+du -sch /*
 
 # Mostrar archivos del sistema
 show_section "Archivos del sistema"
+
 ls -lha /
 
 # Mostrar archivos de la carpeta actual
 show_section "Archivos de carpeta"
+
 ls -lha ./
 
 # Mostrar pie de página
 echo -e "\n/////////////////////////////////////////"
-echo -e "//  Script Desarrollado por JonatanHN  //"
-echo -e "/////////////////////////////////////////\n"
+echo -e "// Script Desarrollado por JonatanHN  //"
+echo -e "///////////////////////////////////////\n"
